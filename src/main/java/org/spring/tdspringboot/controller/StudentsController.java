a3fecd0feaba90d1b712c0cd9559d036b8ffedcd
mathieu <mathieu.user24@gmail.com>
2026-03-18 20:22:57 +0300

    feat(StudentsController): create controller with method [POST and GET] '/students' to create and find student list

--- src/main/java/org/spring/tdspringboot/controller/StudentsController.java
+++ src/main/java/org/spring/tdspringboot/controller/StudentsController.java
@@ -1,7 +1,29 @@
 package org.spring.tdspringboot.controller;
 
+import org.spring.tdspringboot.entity.Student;
+import org.spring.tdspringboot.service.StudentsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentsController {
+    private final StudentsService studentsService;
+
+    public StudentsController(StudentsService studentsService) {
+        this.studentsService = studentsService;
+    }
+
+    @PostMapping("/students")
+    public List<Student> createStudents(@RequestBody List<Student> students) {
+        return studentsService.addStudents(students);
+    }
+
+    @GetMapping("/students")
+    public  List<Student> findAllStudents() {
+        return studentsService.findAllStudents();
+    }
 }
