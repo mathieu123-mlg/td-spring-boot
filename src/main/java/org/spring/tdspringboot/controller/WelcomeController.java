017811d09fcad9db1c5880e25840528e7495e3eb
mathieu <mathieu.user24@gmail.com>
2026-03-18 16:03:29 +0300

    feat(WelcomeController): Add welcome method with name parameter

--- src/main/java/org/spring/tdspringboot/controller/WelcomeController.java
+++ src/main/java/org/spring/tdspringboot/controller/WelcomeController.java
@@ -1,7 +1,14 @@
 package org.spring.tdspringboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam(value = "name") String name) {
+        return "Welcome " + name;
+    }
 }
