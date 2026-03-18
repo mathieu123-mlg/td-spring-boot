44011b5a79a01f86aed1c4cdb59f43b64e0235c1
mathieu <mathieu.user24@gmail.com>
2026-03-18 14:34:12 +0300

    Initial commit

---
+++ src/main/java/org/spring/tdspringboot/TdSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.spring.tdspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/tdspringboot/TdSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.tdspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
