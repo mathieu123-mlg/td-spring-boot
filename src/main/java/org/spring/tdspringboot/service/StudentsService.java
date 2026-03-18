0e9e46588c6d79ce1b0e8b873c753a81afe9901e
mathieu <mathieu.user24@gmail.com>
2026-03-18 20:19:58 +0300

    feat(StudentsService): add method to create and get student list

--- src/main/java/org/spring/tdspringboot/service/StudentsService.java
+++ src/main/java/org/spring/tdspringboot/service/StudentsService.java
@@ -1,4 +1,24 @@
 package org.spring.tdspringboot.service;
 
+import org.spring.tdspringboot.entity.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentsService {
+    private final List<Student> students;
+
+    public StudentsService(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> addStudents(List<Student> studentList) {
+        students.addAll(studentList);
+        return studentList;
+    }
+
+    public List<Student> findAllStudents() {
+        return students;
+    }
 }
