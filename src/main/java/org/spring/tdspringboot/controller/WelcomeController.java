cb5050ec5648381084fbd16a7308991e4b125631
mathieu <mathieu.user24@gmail.com>
2026-03-19 10:54:40 +0300

    feat(WelcomeController): specify status code

--- src/main/java/org/spring/tdspringboot/controller/WelcomeController.java
+++ src/main/java/org/spring/tdspringboot/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package org.spring.tdspringboot.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/welcome")
-    public String welcome(@RequestParam(value = "name") String name) {
-        return "Welcome " + name;
+    public ResponseEntity<String> welcome(@RequestParam(value = "name") String name) {
+        if (name.isEmpty()) {
+            return ResponseEntity
+                    .status(400)
+                    .body("Name is required and cannot be empty");
+        }
+        return ResponseEntity
+                .status(200)
+                .body("Welcome " + name);
     }
 }
