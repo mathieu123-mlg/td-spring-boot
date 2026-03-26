5d487f15af1982c5ca2ee3f0928d1a9e4e7ec178
mathieu <mathieu.user24@gmail.com>
2026-03-26 11:59:42 +0300

    refactore: refactor code using validator and create methode parseAcceptHeader

--- src/main/java/org/spring/tdspringboot/controller/StudentsController.java
+++ src/main/java/org/spring/tdspringboot/controller/StudentsController.java
@@ -2,6 +2,8 @@ package org.spring.tdspringboot.controller;
 
 import org.spring.tdspringboot.entity.Student;
 import org.spring.tdspringboot.service.StudentsService;
+import org.spring.tdspringboot.util.ResponseBuilder;
+import org.spring.tdspringboot.validator.StudentValidator;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,67 +13,60 @@ import java.util.List;
 @RestController
 public class StudentsController {
     private final StudentsService studentsService;
+    private final StudentValidator studentValidator;
+    private final ResponseBuilder responseBuilder;
 
-    public StudentsController(StudentsService studentsService) {
+    public StudentsController(StudentsService studentsService, StudentValidator studentValidator, ResponseBuilder responseBuilder) {
         this.studentsService = studentsService;
+        this.studentValidator = studentValidator;
+        this.responseBuilder = responseBuilder;
     }
 
     @PostMapping("/students")
-    public ResponseEntity<?> createStudents(@RequestBody List<Student> students) {
-        if (students == null || students.isEmpty()) {
-            return ResponseEntity
-                    .status(400)
-                    .header("Content-Type", "application/json")
-                    .body("{\"message\":\"List of student is required\"}");
-        }
+    public ResponseEntity<?> createStudents(@RequestBody List<Student> newStudents) {
         try {
-            return ResponseEntity
-                    .status(201)
-                    .header("Content-Type", "application/json")
-                    .body(studentsService.addStudents(students));
+            studentValidator.validateCreateRequest(newStudents);
+            List<Student> students = studentsService.addStudents(newStudents);
+            return responseBuilder.created(students);
+        } catch (IllegalArgumentException e) {
+            return responseBuilder.badRequest(e.getMessage());
         } catch (RuntimeException e) {
-            return ResponseEntity
-                    .status(500)
-                    .header("Content-Type", "application/json")
-                    .body("{\"message\":\"%s\"}".formatted(e.getMessage()));
+            return responseBuilder.internalError(e.getMessage());
         }
     }
 
     @GetMapping("/students")
-    public ResponseEntity<?> findAllStudents(
-            @RequestHeader(value = "Accept", defaultValue = MediaType.TEXT_PLAIN_VALUE) String acceptHeader
-    ) {
-        if (acceptHeader == null) {
-            return ResponseEntity
-                    .status(400)
-                    .body("{\"message\":\"Accept header is required\"}");
-        }
-        try {
-            if (acceptHeader.equals(MediaType.TEXT_PLAIN_VALUE) ||
-                acceptHeader.equals(MediaType.APPLICATION_JSON_VALUE)) {
+    public ResponseEntity<Object> findAllStudents(
+            @RequestHeader(value = "Accept", defaultValue = MediaType.TEXT_PLAIN_VALUE) String acceptHeader) {
 
-                List<Student> students = studentsService.findAllStudents();
-                if (students.isEmpty()) {
-                    return ResponseEntity.noContent().build();
-                }
-                return ResponseEntity
-                        .status(200)
-                        .header("Content-Type", acceptHeader)
-                        .body(
-                                acceptHeader.equals(MediaType.TEXT_PLAIN_VALUE)
-                                        ? students.toString()
-                                        : students
-                        );
-            } else {
-                return ResponseEntity
-                        .status(501)
-                        .body("{\"message\":\"Format not supported\"}");
+        try {
+            MediaType mediaType = parseAcceptHeader(acceptHeader);
+            List<Student> students = studentsService.findAllStudents();
+            if (students.isEmpty()) {
+                return responseBuilder.noContent();
             }
-        } catch (RuntimeException e) {
+            Object responseBody = mediaType.equals(MediaType.TEXT_PLAIN)
+                    ? students.toString()
+                    : students;
+
             return ResponseEntity
-                    .status(500)
-                    .header("Content-Type", acceptHeader)
-                    .body("{\"message\":\"%s\"}".formatted(e.getMessage()));
+                    .status(200)
+                    .contentType(mediaType)
+                    .body(responseBody);
+        } catch (IllegalArgumentException e) {
+            return responseBuilder.notSupported();
+        } catch (RuntimeException e) {
+            return responseBuilder.internalError(e.getMessage());
         }
+    }
+
+    private MediaType parseAcceptHeader(String acceptHeader) {
+        if (MediaType.TEXT_PLAIN_VALUE.equals(acceptHeader)) {
+            return MediaType.TEXT_PLAIN;
+        }
+        if (MediaType.APPLICATION_JSON_VALUE.equals(acceptHeader)) {
+            return MediaType.APPLICATION_JSON;
+        }
+        throw new IllegalArgumentException("Format not supported");
     }
 }
